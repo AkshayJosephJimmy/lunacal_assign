0c33aba64967664c377a795b819ae17ceaf7e2d8
akshay-joseph-jimmy <akshayjosephjimmy05@gmail.com>
2025-10-23 12:00:06 +0530

    nav bar layout compeleted

--- src/components/DetailsWidget.jsx
+++ src/components/DetailsWidget.jsx
@@ -1,9 +1,12 @@
+import Nav from "./Nav";
 
 
 
 function DetailsWidget() {
   return (
     <div className=" absolute details-widget h-[316px] w-full max-w-[720px] bg-gray-600 ">
+        <Nav/>
+
      
     </div>
   );
---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+
+
+
+function Nav(){
+
+
+
+
+return(
+<div className=" p-1  w-[614px] h-[62px] bg-neutral-900    rounded-[26px] ">
+    <nav className="flex h-full w-full text-gray-400 mx-auto gap-1.5 justify-baseline items-center rounded-[26px]">
+
+    <button className="h-full  w-1/3 rounded-[26px] bg-gray-200">About me</button>
+    <button className="h-[49px] w-1/3 rounded-[26px] bg-gray-200">Experince</button>
+    <button className="h-[49px] w-1/3 rounded-[26px] bg-gray-200">Recommended</button>
+    </nav>
+</div>
+)
+
+}
+export default Nav;
+
+
+
