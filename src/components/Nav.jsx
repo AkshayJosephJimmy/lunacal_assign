cb4750deca71ea2b69d71a456d9d5bc0a1ee8881
akshay-joseph-jimmy <akshayjosephjimmy05@gmail.com>
2025-10-27 19:06:46 +0530

    touch up

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,7 +13,7 @@ function handleSelect(index){
 
 
 return(
-<div className=" p-1 mt-1 w-full w-max-[614px] h-[62px] bg-neutral-900 shadow-2xl shadow-black rounded-[26px] mx-auto  ">
+<div className=" p-1 mt-1  w-[614px] h-[62px] bg-neutral-900 shadow-2xl shadow-black rounded-[26px] mx-auto  ">
     <nav className=" relative flex h-full w-full text-gray-400 mx-auto gap-1.5 justify-baseline items-center rounded-[26px]">
 
         <div
@@ -23,7 +23,7 @@ return(
 ></div>
 
 
-{["About me", "Experince", "Recommended"].map((label,index) => (
+{["About me", "Expereince", "Recommended"].map((label,index) => (
     <button  onClick={()=>handleSelect(index)} key={index} className={`h-full z-10 w-1/3 
        ${selected === index ?`text-white`:"text-gray-400 hover:bg-[length:100%_100%]  transition-all duration-500 ease-out bg-no-repeat  "} rounded-[26px]  bg-linear-to-r from-neutral-850 via-neutral-850 to-neutral-800  bg-[length:0%_100%]  `}>
     {label}
